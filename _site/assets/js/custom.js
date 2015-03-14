b6f9cb67fd56f1a5f95382a575d2a7eda7ecb1e1
Luke Fender <lfender6445@gmail.com>
2015-03-14 14:00:50 -0400

    remove cached index

--- _site/assets/js/custom.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $("a[href^=http]").each(function(){
-    if(this.href.indexOf(location.hostname) == -1) {
-      $(this).attr({
-        target: "_blank"
-      });
-    }
-  });
-});
